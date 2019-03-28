b9e830a2cb769b7da75803546fd51c24d1299e2d
StarForce <46996465+oneframemobile@users.noreply.github.com>
2019-03-28 10:42:52 +0300

    homepage

--- networking-swagger-swift.rb
+++ networking-swagger-swift.rb
@@ -1,5 +1,5 @@
 class NetworkingSwaggerSwift < Formula
-    homepage "https://github.com/oneframemobile/homebrew-networking-swagger-java"
+    homepage "https://github.com/oneframemobile/homebrew-networking-swagger-swift"
     desc "Swagger tool , Networking Library code generator project"
     url "https://github.com/oneframemobile/networking-swagger-swift/archive/1.0.tar.gz"
     sha256 "d8d84f760d9d5911d319282ff8a11a5d70c91ab705c7a04d4ed1c97eda12af1d"
