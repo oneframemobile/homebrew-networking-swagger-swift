10c6ff91f9512e41f627dac80c837fb27f78892a
02483564 <umut.boz@kocsistem.com.tr>
2019-04-08 11:17:26 +0300

    2.1 version fix

--- networking-swagger-swift.rb
+++ networking-swagger-swift.rb
@@ -1,9 +1,9 @@
 class NetworkingSwaggerSwift < Formula
     homepage "https://github.com/oneframemobile/homebrew-networking-swagger-swift"
     desc "Swagger tool , Networking Library code generator project"
-    url "https://github.com/oneframemobile/networking-swagger-swift/archive/2.0.tar.gz"
-    sha256 "97ec8541935430efb4bb55248cac61037c1a8e7fa4ce60b68f38f8a6993fbe54"
-    version "2.0.0"
+    url "https://github.com/oneframemobile/networking-swagger-swift/archive/2.1.tar.gz"
+    sha256 "adf61ad0032c917055e1b3723dd1f741762dba33ee735d1cad592a18249d5cce"
+    version "2.1.0"
     # depends_on "cmake" => :build
     
     def install
